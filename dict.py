e71dcb81344971e1dd10f1a31b370795deb1506b
Asma Abufayah <aaha44@gmail.com>
2022-05-18 09:01:13 +0300

    Homework solved

--- dict.py
+++ dict.py
@@ -2,20 +2,20 @@ Nestle_roducts={"KitKat" : "34,456,432","Nescafe" : "14,106,132","Maggi" : "9,96
 Unilever_products={"Lipton" : "23,456,000","Breyers" : "1,235,891","HellManns" : "17,241,412","Marmite" : "11,715,324"}
 
 for key ,value in Unilever_products.items():
-    print(key,value)
+    print(key,value,"us dollar")
 for key ,value in Nestle_roducts.items():
-    print(key,value)
+    print(key,value,"us dollar")
 
 print("Nestle products are {}".format(len(Nestle_roducts)),"Unilever products are {}".format(len(Unilever_products)))
 
 max_value = max(Nestle_roducts.values())  # maximum value
 max_keys = [k for k, v in Nestle_roducts.items() if v == max_value] 
 
-print("the top selling in Nestle_roducts is",max_value, max_keys)
+print("the top selling in Nestle_roducts is",max_value,"us dollar", max_keys)
 max_value = max(Unilever_products.values())  # maximum value
 max_keys = [k for k, v in Unilever_products.items() if v == max_value] 
 
-print("the top selling in Unilever_products is",max_value, max_keys)
+print("the top selling in Unilever_products is",max_value,"US dollar", max_keys)
 
 
 
