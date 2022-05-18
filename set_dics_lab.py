fd0ade2f0f5c1d77fbfcf04831372ff33ad9c7d4
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-18 08:21:41 +0300

    Update HW

--- set_dics_lab.py
+++ set_dics_lab.py
@@ -1,14 +1,22 @@
 dic_Nestle ={'kitkat':'34,456,432 ','Nescafe':'14,106,132 ','Maggi':'9,960,312 ','Nido':'44,506,003'}
+print("Nestle :")
 for key, value in dic_Nestle.items():
     print(key,':',value)
 
 dic_Unilever ={'Lipton':'23,456,000','Breyers':'1,235,891','HellManns':'17,241,412','Marmite':'11,715,324'}
+print("Unilever :")
 for key, value in dic_Unilever.items():
     print(key,':',value)
 
-new_comp= dic_Nestle,dic_Unilever
-max_length = max(map(len, new_comp))
-print("Nestle and Unilever has the same number of product",[x for x in new_comp if len(x) == max_length])
+
+nestle_pro = len(dic_Nestle)
+unilever_pro =len(dic_Unilever)
+if nestle_pro > unilever_pro:
+    print("Nestle has more product")
+elif unilever_pro > nestle_pro:
+    print("Unilever has more product")
+else:
+    print("Nestle and Unilever has the same number of product")
 
 
 max_sale1 = max(dic_Nestle,key=dic_Nestle.get)
@@ -20,6 +28,11 @@ print("The TOP selling product in Unilever is :",max_sale2)
 citis_Nestle = {"Saudi Arabia", "Oman", "Kuwait", "Egypt", "Jordan", "Sudan"} 
 citis_Unilever ={"Saudi Arabia", "Kuwait", "Iraq", "Morocco", "Yemen", "United Emirates"}
 
-print(citis_Nestle | citis_Unilever) 
-print(citis_Nestle & citis_Unilever)
-print(citis_Nestle - citis_Unilever)
+for n in citis_Nestle.union(citis_Unilever):
+    print(n , end=' - ')
+print("\n")
+for c in citis_Nestle.intersection(citis_Unilever):
+    print(c, end=' - ')
+print("\n")
+for a in citis_Nestle.difference(citis_Unilever):
+    print(a, end=' - ')
