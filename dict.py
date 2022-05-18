7b680fa7902c6a1c213b43f97890638e866ec798
Asma Abufayah <aaha44@gmail.com>
2022-05-18 08:52:23 +0300

    max products added

--- dict.py
+++ dict.py
@@ -1,5 +1,5 @@
-Nestle_roducts={"KitKat" : "34,456,432 US Dollars","Nescafe" : "14,106,132 US Dollars","Maggi" : "9,960,312 US Dollars","Nido" : "44,506,003 US Dollars"}
-Unilever_products={"Lipton" : "23,456,000 US Dollars","Breyers" : "1,235,891 US Dollars","HellManns" : "17,241,412 US Dollars","Marmite" : "11,715,324 US Dollars"}
+Nestle_roducts={"KitKat" : "34,456,432","Nescafe" : "14,106,132","Maggi" : "9,960,312","Nido" : "44,506,003"}
+Unilever_products={"Lipton" : "23,456,000","Breyers" : "1,235,891","HellManns" : "17,241,412","Marmite" : "11,715,324"}
 
 for key ,value in Unilever_products.items():
     print(key,value)
@@ -8,8 +8,12 @@ for key ,value in Nestle_roducts.items():
 
 print("Nestle products are {}".format(len(Nestle_roducts)),"Unilever products are {}".format(len(Unilever_products)))
 
+max_key = max(Nestle_roducts, key=Nestle_roducts.get)
 
+print("the top selling in Nestle_roducts is",max_key)
+max_key2 = max(Unilever_products, key=Unilever_products.get)
 
+print("the top selling in Unilever_products is",max_key)
 
 
 
